6129e4c20e3d3dee70864791188c7ed9b8bc3680
Blabos de Blebe <blabos@blabos.org>
2011-05-26 17:41:32 -0300

    esboço da lógica principal

--- Exp/exp.c
+++ Exp/exp.c
@@ -13,56 +13,48 @@ static int __my_stack_pointer__ = 0;
 int eval(char* expr, int len) {
     int pos = -1;
 
-    do {
-        pos++;
-    } while (is_space(expr[pos]));
-
-    if (is_number(expr[pos])) {
-        goto number;
-    } else if (is_open_parentesis(expr[pos])) {
-        goto push;
-    } else if (is_null(expr[pos])) {
-        goto end;
-    } else {
-        goto error;
-    }
-
-    number: do {
-        pos++;
-    } while (is_space(expr[pos]));
-
-    if (is_operator(expr[pos])) {
-        goto operator;
-    } else if (is_null(expr[pos])) {
-        goto end;
-    } else {
-        goto error;
-    }
-
-    operator: do {
-        pos++;
-    } while (is_space(expr[pos]));
-
-    if (is_number(expr[pos])) {
-        goto number;
-    } else if (is_open_parentesis(expr[pos])) {
-        goto push;
-    } else {
-        goto error;
-    }
-
-    push: do {
-        pos++;
-    } while (is_space(expr[pos]));
-
-    if (!stack_push()) {
-        goto error;
+    begin: pos++;
+    if (is_space(expr[pos])) {
+        goto begin;
     } else if (is_number(expr[pos])) {
+        goto number;
+    } else if (is_open_parentesis(expr[pos])) {
+        goto push;
+    } else if (is_null(expr[pos])) {
+        goto end;
+    } else {
+        goto error;
+    }
+
+    number: pos++;
+    if (is_space(expr[pos])) {
         goto number;
     } else if (is_operator(expr[pos])) {
         goto operator;
-    } else if (is_space(expr[pos])) {
-        goto space;
+    } else if (is_null(expr[pos]) && stack_empty()) {
+        goto end;
+    } else {
+        goto error;
+    }
+
+    operator: pos++;
+    if (is_space(expr[pos])) {
+        goto operator;
+    } else if (is_number(expr[pos])) {
+        goto number;
+    } else if (is_open_parentesis(expr[pos])) {
+        goto push;
+    } else {
+        goto error;
+    }
+
+    push: pos++;
+    if (is_space(expr[pos])) {
+        goto push;
+    } else if (!stack_push()) {
+        goto error;
+    } else if (is_number(expr[pos])) {
+        goto number;
     } else if (is_open_parentesis(expr[pos])) {
         goto push;
     } else {
@@ -70,17 +62,23 @@ int eval(char* expr, int len) {
     }
 
     pop: pos++;
-    if (!stack_pop()) {
+    if (is_space(expr[pos])) {
+        goto pop;
+    } else if (!stack_pop()) {
         goto error;
     } else if (is_operator(expr[pos])) {
         goto operator;
-    } else if (is_space(expr[pos])) {
-        goto space;
     } else if (is_close_parenteis(expr[pos])) {
         goto pop;
+    } else if (is_null(expr[pos]) && stack_empty()) {
+        goto end;
     } else {
         goto error;
     }
+
+    end: return 0;
+
+    error: return -1;
 }
 
 int stack_push() {
@@ -105,5 +103,9 @@ int stack_pop() {
     }
 
     return 0;
+}
+
+int stack_empty() {
+    return !!__my_stack_pointer__;
 }
 
